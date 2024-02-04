2a3bae98a4e03f56ec79bec1605682ecd472082a
DanHak59 <144971743+danyalhaque@users.noreply.github.com>
2024-02-03 20:59:21 -0500

    components added

---
+++ frontend/src/components/Test.js
@@ -0,0 +1,16 @@
+// Filename - Test.js
+
+import React from 'react'
+import "./Test.css"
+
+const Test = () => {
+  return (
+    <div className='Test'>
+        <header className='Test-Header'>
+        <h1>Testing Text</h1>
+        </header>
+    </div>
+  )
+}
+
+export default Test
