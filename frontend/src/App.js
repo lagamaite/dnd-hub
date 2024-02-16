9f936810ca9c975f44885d61bb96e7b53ace6b07
Lucy Agamaite <lucy.agamaite@gmail.com>
2024-02-16 01:11:35 -0500

    added blog template

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,11 +5,12 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import StatComponent from "./components/StatComponent"
 import Test from "./components/Test"
+import Blog from "./components/blog/Blog"
 
 function App() {
     // useState for setting an array of characters
     
-return(<div><StatComponent/><Test/></div>)
+return(<div><StatComponent/><Test/><Blog/></div>)
 
 }
 
