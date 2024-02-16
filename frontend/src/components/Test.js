8d9bf219176c90c6bffe028d2dfb592cca8a80ce
DanHak59 <144971743+danyalhaque@users.noreply.github.com>
2024-02-15 22:09:12 -0500

    Changed test.js to a map page.

--- frontend/src/components/Test.js
+++ frontend/src/components/Test.js
@@ -1,13 +1,31 @@
 // Filename - Test.js
 
-import React from 'react'
+import React, {useState} from 'react'
 import "./Test.css"
 
+
 const Test = () => {
+  const [selectedImage, setSelectedImage] = useState('Black_Desert.png');
+
+  const handleImageChange = (event) => {
+    setSelectedImage(event.target.value);
+  };
+
   return (
     <div className='Test'>
         <header className='Test-Header'>
-        <h1>Testing Text</h1>
+
+        <h1>Maps</h1>
+        <div>
+          <label htmlFor="imageDropdown">Select Map: </label>
+          <select id="imageDropdown" value={selectedImage} onChange={handleImageChange}>
+            <option value="Black_Desert.png">Black Desert</option>
+            <option value="Ivory_Desert.png">Ivory Desert</option>
+          </select>
+        </div>
+        <picture>
+          <img className='Map-Image' src={require(`../Images/${selectedImage}`)} alt="" />
+        </picture>
         </header>
     </div>
   )
